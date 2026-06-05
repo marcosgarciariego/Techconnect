4c8051a7e851b7f065aa0849f3d7e2c7f7bca480
marcosgarciariego <marcosgarciariego@gmail.com>
2026-06-05 12:44:02 +0200

    Configure Astro deployment for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [
@@ -10,9 +10,7 @@ export default defineConfig({
     tailwind(),
   ],
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   server: {
     port: 4321,
     host: true,
